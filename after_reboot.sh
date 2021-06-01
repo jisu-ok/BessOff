7f350a2a36c8fe56210be513484956d36fd1ba1d
Jisu <jisu.ok@kaist.ac.kr>
2021-06-02 00:26:10 +0900

    add script for setting VFIO no-IOMMU mode

--- after_reboot.sh
+++ after_reboot.sh
@@ -5,3 +5,9 @@
 echo 1024 | sudo tee /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
 echo 1024 | sudo tee /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
 
+# Enable no-IOMMU mode
+sudo modprobe vfio-pci
+echo 1 | sudo tee /sys/module/vfio/parameters/enable_unsafe_noiommu_mode
+
+sudo bin/dpdk-devbind.py --status
+
