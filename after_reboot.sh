e5dde2cc4397a4baaafaa825684f9cada03cd377
Jisu <jisu.ok@kaist.ac.kr>
2021-07-08 15:51:09 +0900

    add script for 1GB hugepage setting

--- after_reboot.sh
+++ after_reboot.sh
@@ -1,9 +1,14 @@
 # For single-node systems
 #sudo sysctl vm.nr_hugepages=1024
 
-# For multi-node (NUMA) systems
-echo 1024 | sudo tee /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
-echo 1024 | sudo tee /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
+# For multi-node (NUMA) systems - 2KB hugepage
+# echo 1024 | sudo tee /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
+# echo 1024 | sudo tee /sys/devices/system/node/node1/hugepages/hugepages-2048kB/nr_hugepages
+
+# For multi-node (NUMA) systems - 1GB hugepage
+echo 8 | sudo tee /sys/devices/system/node/node0/hugepages/hugepages-1048576kB/nr_hugepages
+echo 8 | sudo tee /sys/devices/system/node/node1/hugepages/hugepages-1048576kB/nr_hugepages
+
 
 # Enable no-IOMMU mode
 sudo modprobe vfio-pci
